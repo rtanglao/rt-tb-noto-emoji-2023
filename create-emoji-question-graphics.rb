bd255f958f9586c0f29109396a00bbb4e4e6dcc9
Roland Tanglao <roland@rolandtanglao.com>
2023-09-04 08:17:01 -0700

    penguin works :-)

---
+++ create-emoji-question-graphics.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'bundler/setup'
+require 'amazing_print'
+require 'json'
+require 'time'
+require 'date'
+require 'csv'
+require 'logger'
+require 'rmagick'
+logger = Logger.new($stderr)
+logger.level = Logger::DEBUG
+if ARGV.length < 2
+  puts "usage: #{$PROGRAM_NAME} <questions_CSV_file> <answers_CSV_file>"
+  exit
+end
+
+all_questions = CSV.read(ARGV[0], headers: true)
+all_answers = CSV.read(ARGV[1], headers: true)
+all_questions.each do |q|
+  content = "#{q['title']} #{q['content']}"
+  question_creator = q['creator']
+  all_answers.each do |a|
+    content += " #{a['content']}" if a['creator'] == question_creator
+  end
+  content += " #{q['tags']}"
+  emoji = '🐧'
+  image = Magick::Image.read("pango:<span font='Noto Color Emoji'><b>OS:</b>#{emoji}</span>").first
+image.write('pango_sample1.png')
+ap content
+exit
+end
